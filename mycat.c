f7ad5f4045527b8b1756598ea4e40dfa75515e80
tjrbwls <118953733+tjrbwls@users.noreply.github.com>
2023-06-09 23:51:43 +0900

    Update mycat.c

--- mycat.c
+++ mycat.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	FILE *fp;
-	int c;
+int main(int argc, char *argv[]){
+    int i;
 
-	if (argc < 2)
-		fp = stdin;
-	else
-		fp = fopen(argv[1], "r");
+    for(i = 1; i<argc; i++){
+        FILE *f;
+        int c;
 
-	c = getc(fp);
-	while (c != EOF) {
-		putc(c, stdout);
-		c = getc(fp);
-	}
-	fclose(fp);
-	return 0;
+        f = fopen(argv[i], "r");
+        if(!f){
+            perror(argv[i]);
+            exit(1);
+        }
+
+        while((c=fgetc(f))!=EOF){
+            if(putchar(c) < 0) 
+		exit(1);
+        }
+        fclose(f);
+    }
+        exit(0);
+    }
 }
